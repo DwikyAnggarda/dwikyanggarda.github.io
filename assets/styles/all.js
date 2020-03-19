1370b5a7f6862bbe64ca78ba464e2e0f46ad747a
DwikyAnggarda <dwiky.amin@gmail.com>
2020-03-20 00:08:12 +0700

    Commit ke - 2

---
+++ assets/styles/all.js
@@ -0,0 +1,14 @@
+//Membuka aplikasi NGWOTA
+function html5Music() {
+    window.open("https://ngwota.herokuapp.com/");
+}
+
+//Membuka aplikasi Game Tebak Koin
+function tebakKoin() {
+    window.open("#");
+}
+
+//Coming Soon!
+function comingSoon() {
+    location.replace("#")
+}
